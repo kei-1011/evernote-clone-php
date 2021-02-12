08f3f3fc660df43f85c9057a77a36ef5d6c3cfd3
kei-1011 <k_kojima0407@yahoo.co.jp>
2021-02-12 22:19:06 +0900

    login

--- user/action/register.php
+++ user/action/register.php
@@ -1,10 +1,44 @@
 <?php
+session_start();
+require '../../common/validation.php';
 require '../../common/database.php';
 
 // パラメータ
 $user_name = $_POST['user_name'];
 $user_email = $_POST['user_email'];
 $user_password = $_POST['user_password'];
+
+// バリデーション
+$_SESSION['errors'] = [];
+
+// - 空チェック
+empty_check($_SESSION['errors'], $user_name, "ユーザー名を入力してください。");
+empty_check($_SESSION['errors'], $user_email, "メールアドレスを入力してください。");
+empty_check($_SESSION['errors'], $user_password, "パスワードを入力してください。");
+
+// - 文字数チェック
+string_max_size_check($_SESSION['errors'], $user_name, "ユーザー名は255文字以内で入力してください。");
+string_max_size_check($_SESSION['errors'], $user_email, "メールアドレスは255文字以内で入力してください。");
+string_max_size_check($_SESSION['errors'], $user_password, "パスワードは255文字以内で入力してください。");
+string_min_size_check($_SESSION['errors'], $user_password, "パスワードは8文字以上で入力してください。");
+
+if(!$_SESSION['errors']) {
+  // - メールアドレスチェック
+  mail_address_check($_SESSION['errors'], $user_email, "正しいメールアドレスを入力してください。");
+
+  // - ユーザー名・パスワード半角英数チェック
+  half_alphanumeric_check($_SESSION['errors'], $user_name, "ユーザー名は半角英数字で入力してください。");
+  half_alphanumeric_check($_SESSION['errors'], $user_password, "パスワードは半角英数字で入力してください。");
+
+  // - メールアドレス重複チェック
+  mail_address_duplication_check($_SESSION['errors'], $user_email, "既に登録されているメールアドレスです。");
+
+}
+
+if($_SESSION['errors']) {
+    header('Location: ../../user/');
+    exit;
+}
 
 // DB接続処理
 $database_handler = get_database_connection();
@@ -17,6 +51,12 @@ try {
     $statement->bindParam(':email',$user_email);
     $statement->bindParam(':password', $password);
     $statement->execute();
+
+    $_SESSION['user'] = [
+      'name' => $user_name,
+      'id' => $database_handler->lastInsertId(),
+    ];
+
   }
 } catch (Throwable $e) {
   echo $e->getMessage();
